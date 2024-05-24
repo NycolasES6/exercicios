4254c728f32e5c62c3365b58f8a59bb14885f2fc
NycolasES6 <nycolasonrails@gmail.com>
2024-05-24 19:21:44 -0300

    powershell - getting started - objects

---
+++ powershell/getting-started/criando-objetos/criando-armazenando-objetos.ps1
@@ -0,0 +1,13 @@
+$newObject = New-Object -TypeName psobject -Property @{
+    ComputerName = 'SERVER'
+    OS = 'Windows x64'
+    Environment = 'Production'
+}
+
+Write-Output $newObject
+
+$otherObject = [PSCustomObject]@{
+    ComputerName = "SERVER2"
+    OS = 'Ubuntu 24.04 LTS'
+    Enviq
+}
