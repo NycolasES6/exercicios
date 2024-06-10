a604cc7f7d64c9554758debd3064c134abf60f78
Nycolas Ubuntu Server <nycolasonrails@gmail.com>
2024-06-10 19:13:16 +0000

    10/06 - 2

---
+++ bash/exercicios/htb - operadores/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+var="8dm7KsjU28B7v621Jls"
+
+value="ERmFRMVZ0U2paTlJYTkxDZz09Cg"
+
+for i in {1..40}
+do
+    var=$(echo "$var" | base64)
+
+    if echo "$variavel" |  '*\-*'
+    then
+        echo "$var"
+    fi
+
+done
