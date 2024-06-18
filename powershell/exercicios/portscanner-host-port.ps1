650ba091c428ce148f7bb63a2403e06268e4305b
NycolasES6 <nycolasonrails@gmail.com>
2024-06-18 18:39:16 -0300

    18/06

--- powershell/exercicios/portscanner-host-port.ps1
+++ powershell/exercicios/portscanner-host-port.ps1
@@ -5,8 +5,8 @@ if (!$ip -or !$porta) {
     Write-Host "Modo de uso: .\script.ps1 <ip> <porta>"
 }else{
     if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
-        Write-Host "Porta ABERTA"
+        Write-Host "Porta $porta ABERTA"
     }else {
-        Write-Host "Porta FECHADA"
+        Write-Host "Porta $porta FECHADA"
     }
 }
